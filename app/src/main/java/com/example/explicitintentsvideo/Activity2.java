643453efe954267388cffe5a4d20a0118248c5fd
danglvh1004 <lvhd.hdlv@gmail.com>
2021-01-09 21:57:30 +0700

    Commit

---
+++ app/src/main/java/com/example/explicitintentsvideo/Activity2.java
@@ -0,0 +1,24 @@
+package com.example.explicitintentsvideo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Activity2 extends AppCompatActivity {
+
+    TextView tvWelcome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        tvWelcome = findViewById(R.id.tvWelcome);
+
+        String name = getIntent().getStringExtra("data_name");
+
+        tvWelcome.setText(name + ", welcome to Activity 2!");
+    }
+}
